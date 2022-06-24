fd3478ae59322808b139077f02677a395257ec5d
Oswin Bult <obult@student.codam.nl>
2022-06-24 22:40:37 +0200

    extended main for harl testing

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -5,4 +5,8 @@ int     main(void) {
     Harl    karen;
 
     karen.complain("DEBUG");
+    karen.complain("BULLSHIT");
+    karen.complain("WARNING");
+    karen.complain("INFO");
+    karen.complain("ERROR");
 }
